b2cd78d5a78370f2327538398658034722714214
a.yankovich <a.yankovich@rtp.by>
2018-10-24 17:26:42 +0300

    add recursive search

--- CQtDeployer/main.cpp
+++ CQtDeployer/main.cpp
@@ -3,14 +3,14 @@
  * Distributed under the lgplv3 software license, see the accompanying
  * Everyone is permitted to copy and distribute verbatim copies
  * of this license document, but changing it is not allowed.
-*/
+ */
 
-#include <QCoreApplication>
+#include "deploy.h"
 #include "quasarapp.h"
+#include <QCoreApplication>
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
-#include "deploy.h"
 #include <QList>
 
 void help() {
@@ -19,38 +19,50 @@ void help() {
     qInfo() << "";
     qInfo() << "Options:";
     qInfo() << "   help / h                 : show help.";
-    qInfo() << "   always-overwrite         : Copy files even if the target file exists.";
+    qInfo() << "   always-overwrite         : Copy files even if the target "
+               "file exists.";
     qInfo() << "   -bin    [params]         : deployment binry.";
-    qInfo() << "   -qmlDir [params]         : qml datadir of project. for example -qmlDir ~/my/project/qml";
+    qInfo() << "   -qmlDir [params]         : qml datadir of project. for "
+               "example -qmlDir ~/my/project/qml";
     qInfo() << "   deploy-not-qt            : deploy all libs";
     qInfo() << "   -qmake  [params]         : qmake path. for example";
-    qInfo() << "                            | -qmake ~/Qt/5.11.1/gcc_64/bin/qmake";
+    qInfo()
+        << "                            | -qmake ~/Qt/5.11.1/gcc_64/bin/qmake";
     qInfo() << "   -ignore [list,params]    : ignore filter for libs";
-    qInfo() << "                            | for example -ignore libicudata.so.56,libicudata2.so.56";
+    qInfo() << "                            | for example -ignore "
+               "libicudata.so.56,libicudata2.so.56";
     qInfo() << "   clear                    : delete all old deploy data";
-    qInfo() << "   -runScript [params]      : set new name of out file (AppRun.sh by default)";
+    qInfo() << "   -runScript [params]      : set new name of out file "
+               "(AppRun.sh by default)";
     qInfo() << "                            | for example -runScript myApp.sh";
-    qInfo() << "   allQmlDependes           : This flag will force to extract all qml libraries.";
-    qInfo() << "                            | (not recommended, as it takes up a lot of memory)";
-    qInfo() << "   -libDir [list,params]    : set additional path for extralib of app.";
-    qInfo() << "                            | for example -libDir ~/myLib,~/newLibs";
-    qInfo() << "   -extraPlugin[list,params]: set additional path for extraPlugin of app";
+    qInfo() << "   allQmlDependes           : This flag will force to extract "
+               "all qml libraries.";
+    qInfo() << "                            | (not recommended, as it takes up "
+               "a lot of memory)";
+    qInfo() << "   -libDir [list,params]    : set additional path for extralib "
+               "of app.";
+    qInfo() << "                            | for example -libDir "
+               "~/myLib,~/newLibs";
+    qInfo() << "   -extraPlugin[list,params]: set additional path for "
+               "extraPlugin of app";
+    qInfo() << "   recursiveDepth           : set Depth for recursive search "
+               "of libs (default 1)";
+
     qInfo() << "   verbose                  : show debug log";
 
-
     qInfo() << "";
-    qInfo() << "Example: cqtdeployer -bin myApp -qmlDir ~/Qt/5.11.1/gcc_64/qml -qmake ~/Qt/5.11.1/gcc_64/bin/qmake clear";
+    qInfo() << "Example: cqtdeployer -bin myApp -qmlDir ~/Qt/5.11.1/gcc_64/qml "
+               "-qmake ~/Qt/5.11.1/gcc_64/bin/qmake clear";
     qInfo() << "Example (only C libs): cqtdeployer -bin myApp clear";
-
 }
 
-void verboseLog(const QString& str) {
+void verboseLog(const QString &str) {
     if (QuasarAppUtils::Params::isEndable("verbose")) {
         qDebug() << str;
     }
 }
 
-bool parseQt(Deploy& deploy) {
+bool parseQt(Deploy &deploy) {
 
     auto bin = QuasarAppUtils::Params::getStrArg("bin");
 
@@ -68,13 +80,27 @@ bool parseQt(Deploy& deploy) {
         return false;
     }
 
+    int limit = 1;
+
+    if (QuasarAppUtils::Params::isEndable("recursiveDepth")) {
+        bool ok;
+        limit = QuasarAppUtils::Params::getArg("recursiveDepth").toInt(&ok);
+        if (!ok) {
+            limit = 1;
+            qWarning() << "recursiveDepth is invalid!";
+        }
+    }
+
+    deploy.setDepchLimit(limit);
+
     if (QuasarAppUtils::Params::isEndable("clear")) {
         qInfo() << "clear old data";
         deploy.clear();
     }
 
     auto listLibDir = QuasarAppUtils::Params::getStrArg("libDir").split(",");
-    auto listExtraPlugin = QuasarAppUtils::Params::getStrArg("extraPlugin").split(",");
+    auto listExtraPlugin =
+        QuasarAppUtils::Params::getStrArg("extraPlugin").split(",");
     deploy.setExtraPath(listLibDir);
     deploy.setExtraPlugins(listExtraPlugin);
 
@@ -108,7 +134,7 @@ bool parseQt(Deploy& deploy) {
     } else if (QuasarAppUtils::Params::isEndable("allQmlDependes")) {
         deploy.setDeployQml(true);
     } else {
-        qCritical () << "wrong qml dir!";
+        qCritical() << "wrong qml dir!";
     }
 
     if (!dir.cdUp()) {
@@ -119,18 +145,16 @@ bool parseQt(Deploy& deploy) {
 
     return true;
 }
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
     if (!QuasarAppUtils::Params::parseParams(argc, argv)) {
         qWarning() << "wrong parametrs";
         help();
         exit(0);
-
     };
 
     if (QuasarAppUtils::Params::isEndable("h") ||
-            QuasarAppUtils::Params::isEndable("help")) {
+        QuasarAppUtils::Params::isEndable("help")) {
         help();
         exit(0);
     }
