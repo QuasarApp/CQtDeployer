e29aaa80a09a5590a14c2947be0c5f95215e36a3
a.yankovich <a.yankovich@rtp.by>
2018-11-15 16:01:16 +0300

    fix build snap

--- staticBuild.sh
+++ staticBuild.sh
@@ -5,7 +5,15 @@ declare -a QTLIBS
 BASE_DIR=$(dirname "$(readlink -f "$0")")
 QTLIBS=( libQt5Sql.a libQt5Xml.a libQt5Core.a libQt5Test.a libQt5Network.a libQt5Bootstrap.a libQt5Concurrent.a)
 
-RELEASE_DIR=$BASE_DIR/build/release
+RELEASE_DIR=$BASE_DIR/distro
+
+if [ -e "$PREFIX"]
+then
+    echo "PREFIX is empty, use default install path $RELEASE_DIR"
+else
+    echo "use PREFIX path!"
+    RELEASE_DIR=$PREFIX
+fi
 
 cd $BASE_DIR
 
@@ -14,6 +22,8 @@ git submodule update --init --recursive
 make clean
 find $BASE_DIR -type f -name 'Makefile' -exec rm {} \;
 rm $BASE_DIR/QuasarAppLib/Makefile.QuasarApp
+rm -rdf $RELEASE_DIR
+
 cd $BASE_DIR/qtBase
 
 for var in "${QTLIBS[@]}"
@@ -39,8 +49,6 @@ $BASE_DIR/staticQt/bin/qmake QMAKE_LFLAGS+="-static" $BASE_DIR/CQtDeployer.pro
 
 make -j$(nproc)
 
-make install -j$(nproc)
-
 if [ $? -eq 0 ]
 then    
     echo ""
@@ -49,6 +57,10 @@ else
     echo ""
     echo "Build is failed!" >&2
 fi
+
+mkdir -p $RELEASE_DIR
+
+cp $BASE_DIR/build/release/cqtdeployer $RELEASE_DIR/
 
 
 
