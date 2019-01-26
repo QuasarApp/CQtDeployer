b81a05ab832d1694f740961fa506800bb852647a
EndrII <EndrIIMail@gmail.com>
2019-01-26 07:54:56 +0300

    update copyright

--- staticBuild.sh
+++ staticBuild.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2018-2019 QuasarApp.
+# Distributed under the lgplv3 software license, see the accompanying
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+#
+
 IS_SNAP="$1"
 QT_DIR=staticQt
 
