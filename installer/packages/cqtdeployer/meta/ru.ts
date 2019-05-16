9c65c2e443de4924e28725eb65027d59429204e5
Andrei <EndrIIMail@gmail.com>
2019-05-16 13:36:36 +0300

    translate installer

--- installer/packages/cqtdeployer/meta/ru.ts
+++ installer/packages/cqtdeployer/meta/ru.ts
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>installscript</name>
     <message>
         <location filename="installscript.js" line="7"/>
         <source>CQtDeployer</source>
-        <translation type="unfinished"></translation>
+        <translation>CQtDeployer</translation>
     </message>
     <message>
         <location filename="installscript.js" line="8"/>
         <source>This package contains CQtDeployer</source>
         <oldsource>This package contains Snake</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Этот пакет содержит CQtDeployer</translation>
     </message>
 </context>
 </TS>
