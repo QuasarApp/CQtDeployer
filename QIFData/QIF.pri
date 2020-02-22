b892e6474a612f36f07b1835330025284cd82f0f
EndrII <EndrIIMail@gmail.com>
2020-02-22 14:26:54 +0300

    fix windows qif

--- QIFData/QIF.pri
+++ QIFData/QIF.pri
@@ -14,9 +14,9 @@ qif.commands= $$PY $$PWD/scripts/QIF.py $$PLATFORM 3.2.0 $$PWD/packages/QIF/data
 
 !isEmpty( ONLINE ) {
 
-    unix:message(prepare release QIF)
-    unix:deployOffline.depends += qif
-    unix:buildSnap.depends += qif
+    message(prepare release QIF)
+    deployOffline.depends += qif
+    buildSnap.depends += qif
 }
 
 QMAKE_EXTRA_TARGETS += \
--- QIFData/scripts/QIF.py
+++ QIFData/scripts/QIF.py
@@ -8,7 +8,7 @@ VERSION = sys.argv[2]
 PATHQIF = sys.argv[3]
 
 if len(PLATFORM) <= 0:
-    PLATFORM=linux
+    PLATFORM = "linux"
 
 if os.path.isdir(PATHQIF + "/QIF"):
     print("if target already executed")
@@ -18,8 +18,17 @@ print("PLATFORM=" + PLATFORM)
 print("VERSION=" + VERSION)
 
 
-subprocess.call(["aqt","tool", "--outputdir", PATHQIF, PLATFORM, "tools_ifw", VERSION, " "])
+subprocess.call(["aqt",
+                 "tool",
+                 "--outputdir",
+                 PATHQIF,
+                 PLATFORM,
+                 "tools_ifw",
+                 VERSION,
+                 " "])
+
 os.makedirs(PATHQIF + "/QIF")
-os.rename(PATHQIF + "/Tools/QtInstallerFramework/" + VERSION[0:3] + "/bin/", PATHQIF + "/QIF")
+os.rename(PATHQIF + "/Tools/QtInstallerFramework/" + VERSION[0:3] + "/bin/",
+          PATHQIF + "/QIF")
 shutil.rmtree(PATHQIF + "/Tools")
 os.remove("aqtinstall.log")
