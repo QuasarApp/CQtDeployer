9b878bf0e39350e34ef507daeded9576b24659fa
EndrII <EndrIIMail@gmail.com>
2020-03-19 18:00:16 +0300

    fix tests

--- UnitTests/tst_deploytest.cpp
+++ UnitTests/tst_deploytest.cpp
@@ -2208,7 +2208,7 @@ void deploytest::testOutDirs() {
 
     QVERIFY(runScript.contains("SET BASE_DIR=%~dp0"));
     QVERIFY(runScript.contains("SET PATH=%BASE_DIR%\\lolLib\\;%PATH%"));
-    QVERIFY(runScript.contains("start \"\" \"%BASE_DIR%\\lol\\TestQMLWidgets.exe\" %*"));
+    QVERIFY(runScript.contains("call \"%BASE_DIR%\\lol\\TestQMLWidgets.exe\" %*"));
 
 
 #endif
