ee5c18f121c70d398066043cc14a08d79b1491e7
EndrII <EndrIIMail@gmail.com>
2021-01-05 13:17:11 +0300

    update copyrigh update zip lib update QuasarApp lib

--- Deploy/distromodule.cpp
+++ Deploy/distromodule.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2018-2021 QuasarApp.
+ * Distributed under the lgplv3 software license, see the accompanying
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ */
+
 #include "deployconfig.h"
 #include "deploycore.h"
 #include "distromodule.h"
