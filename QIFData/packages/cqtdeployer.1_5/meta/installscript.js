036728ee761c052b7ad18690d6f63eb727cce23a
EndrII <EndrIIMail@gmail.com>
2021-07-15 15:23:33 +0300

    ref #628 "use execute commnad "

--- QIFData/packages/cqtdeployer.1_5/meta/installscript.js
+++ QIFData/packages/cqtdeployer.1_5/meta/installscript.js
@@ -87,19 +87,12 @@ function systemIntegration() {
     console.log("hometDir "  + homeDir)
 
     if (systemInfo.kernelType === "winnt") {
-        component.addElevatedOperation('EnvironmentVariable',
-                               [
-                                   "cqtdeployer",
-                                   targetDir + "\\" + VERSION + "\\cqtdeployer.bat"
-                               ]
-                              )
 
-        component.addElevatedOperation('EnvironmentVariable',
-                               [
-                                   "cqtDir",
-                                   targetDir + "\\" + VERSION + "\\"
-                               ]
-                              )
+        component.addOperation('Execute', ["SETX", "cqtdeployer", targetDir + "\\" + VERSION + "\\cqtdeployer.bat"],
+                               "UNDOEXECUTE", ["SETX", "cqtdeployer="])
+        component.addOperation('Execute', ["SETX", "cqtDir", targetDir + "\\" + VERSION + "\\"],
+                               "UNDOEXECUTE", ["SETX", "cqtDir="])
+
 
         let PATH = installer.environmentVariable("PATH");
         const cqtDir = installer.environmentVariable("cqtDir");
